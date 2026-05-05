c538309aa0b1c7f8dba714084893c04df7a6569c
Samuel Roberts <20165449@tafe.wa.edu.au>
2026-05-05 16:00:17 +0800

    fix: Reopened the file for the first time and it did something.

--- DDA_Week12/obj/Debug/net8.0-windows/DDA_Week12.AssemblyInfo.cs
+++ DDA_Week12/obj/Debug/net8.0-windows/DDA_Week12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDA_Week12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98c3359d9c87fc5b7d7c64fccdd9bae1ad7f435")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDA_Week12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDA_Week12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
